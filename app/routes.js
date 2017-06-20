33b0698c14fca20963b8521811aea145f0ca3692
Argi <argishtiavetis@gmail.com>
2017-06-19 22:06:25 -0700

    df

--- app/routes.js
+++ app/routes.js
@@ -261,7 +261,7 @@ module.exports = function(app, passport) {
 
     app.post('/login', passport.authenticate('local-login', {
         successRedirect : '/profile', // redirect to the secure profile section
-        failureRedirect : '/login', // redirect back to the signup page if there is an error
+        failureRedirect : '/', // redirect back to the signup page if there is an error
         failureFlash : true // allow flash messages
     }));
 
